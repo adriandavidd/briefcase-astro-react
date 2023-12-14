08b2ec1f36b466179391383df6c14b6d69be598f
adrian <adrianurielvergaraa@gmail.com>
2023-12-13 20:19:53 -0500

    hover

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,14 @@
 const Project = () => {
   return (
     <div className="flex items-center justify-between mx-60 gap-10">
-      <div className="bg-zinc-950 border-2 border-zinc-900 p-28 rounded-lg ">
-        <p className="text-white font-bold text-lg">soon...</p>
+      <div className="bg-zinc-950 hover:bg-zinc-900 border-2 border-zinc-900 p-28 rounded-lg ">
+        <p className="text-white font-bold text-2xl">soon...</p>
       </div>
-      <div className="bg-zinc-950 border-2 border-zinc-900 p-28 rounded-lg ">
-        <p className="text-white font-bold text-lg">soon...</p>
+      <div className="bg-zinc-950 hover:bg-zinc-900 border-2 border-zinc-900 p-28 rounded-lg ">
+        <p className="text-white font-bold text-2xl">soon...</p>
       </div>
-      <div className="bg-zinc-950 border-2 border-zinc-900 p-28 rounded-lg ">
-        <p className="text-white font-bold text-lg">soon...</p>
+      <div className="bg-zinc-950 hover:bg-zinc-900 border-2 border-zinc-900 p-28 rounded-lg ">
+        <p className="text-white font-bold text-2xl">soon...</p>
       </div>
     </div>
   );
